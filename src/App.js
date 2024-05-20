ce7cfdb4409a6197bb1c60a8d139032743563ee3
Hi <hi.pham25@tudent.passerellesnumeriques.org>
2024-05-20 21:02:51 +0700

    set up folder

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Navbar from "./components/layout/Navbar";
+function App() {
+return (
+<div className="App">
+<Navbar />
+<div className="container">
+<h1>Hello from React</h1>
+</div>
+</div>
+);
+}
+
+export default App;

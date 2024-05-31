568722a90dca4b933bdbc91ac9167f90a92f60b7
Hi <hi.pham25@tudent.passerellesnumeriques.org>
2024-05-31 08:38:44 +0700

    Build About and NotFound page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import axios from "axios";
-import { useEffect } from "react";
-import { useState } from "react";
 import React from "react";
 import "./App.css";
 import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import Search from "./components/users/Search";
 import Navbar from "./components/layout/Navbar";
-import Users from "./components/users/Users";
+import About from "./components/pages/About";
+import NotFound from "./components/pages/NotFound";
 const App = () => {
   return (
     <div className="App">
@@ -16,6 +14,8 @@ const App = () => {
           <h1>GitHub Users Data</h1>
           <Switch>
             <Route exact path="/" component={Search} />
+            <Route exact path="/about" component={About} />
+            <Route path="/*" component={NotFound}></Route>
           </Switch>
         </div>
       </Router>

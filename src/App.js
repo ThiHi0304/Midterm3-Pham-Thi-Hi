fad2899f8f3d1c3548420658166c7a9e3aaff849
Hi <hi.pham25@tudent.passerellesnumeriques.org>
2024-06-03 08:39:56 +0700

    Minimize App.js file

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
 import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
-import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
-import Search from "./components/users/Search";
+import Home from "./components/layout/Home";
 import Navbar from "./components/layout/Navbar";
-import About from "./components/pages/About";
-import NotFound from "./components/pages/NotFound";
-import User from "./components/users/User";
 const App = () => {
   return (
     <div className="App">
       <Router>
         <Navbar />
-        <div className="container">
-          <h1>GitHub Users Data</h1>
-          <Switch>
-            <Route exact path="/" component={Search} />
-            <Route exact path="/about" component={About} />
-            {/* Add this line to specify the routing*/}
-            <Route exact path="/user/:id" component={User} />
-            <Route path="/*" component={NotFound}></Route>
-          </Switch>
-        </div>
+        <Home />
       </Router>
     </div>
   );

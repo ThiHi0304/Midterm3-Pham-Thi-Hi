0413655c11557c63f55339d8eb8ceed015079672
Hi <hi.pham25@tudent.passerellesnumeriques.org>
2024-06-03 08:27:38 +0700

    User detail

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Search from "./components/users/Search";
 import Navbar from "./components/layout/Navbar";
 import About from "./components/pages/About";
 import NotFound from "./components/pages/NotFound";
+import User from "./components/users/User";
 const App = () => {
   return (
     <div className="App">
@@ -15,6 +16,8 @@ const App = () => {
           <Switch>
             <Route exact path="/" component={Search} />
             <Route exact path="/about" component={About} />
+            {/* Add this line to specify the routing*/}
+            <Route exact path="/user/:id" component={User} />
             <Route path="/*" component={NotFound}></Route>
           </Switch>
         </div>

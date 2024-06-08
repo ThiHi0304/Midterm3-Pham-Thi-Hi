a8c871dfeb700c368c18b0029b6c282a0567f9b9
Hi <hi.pham25@tudent.passerellesnumeriques.org>
2024-06-08 21:42:10 +0700

    fix update deploy

--- src/api.js
+++ src/api.js
@@ -1,9 +1,5 @@
 import axios from 'axios';
-
 const axiosService  = axios.create({
     baseURL:'https://api.github.com'
 });
-
-axiosService.defaults.headers.common['Authorization'] = 'AUTH TOKEN FROM INSTANCE';
-axiosService.defaults.headers.post['Content-Type'] = 'application/json';
 export default axiosService;
